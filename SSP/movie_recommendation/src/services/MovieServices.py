adabdf655a5d71cc15de3b15ea8462d7ef787aeb
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2017-12-05 01:14:32 +0700

    version 0.0.01

---
+++ SSP/movie_recommendation/src/services/MovieServices.py
@@ -0,0 +1,17 @@
+'''
+Created on 29-11-2560
+
+@author: Piyawat Pemwattana
+'''
+
+class MovieServices(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
