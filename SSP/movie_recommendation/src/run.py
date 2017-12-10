a346ce50e7d097be7a6421f4bbb125924b524634
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2017-12-11 06:11:35 +0700

    version 0.0.01

--- SSP/movie_recommendation/src/run.py
+++ SSP/movie_recommendation/src/run.py
@@ -1,7 +1,13 @@
-from random import randint
+'''
+Created on Dec 11, 2017
+
+@author: Piyawat Pemwattana
+'''
 
 from flask import Flask, render_template, jsonify
+from random import randint
 from flask_cors import CORS
+import requests
 
 
 app = Flask(__name__,
@@ -9,16 +15,20 @@ app = Flask(__name__,
             template_folder = "./dist")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/random')
-def random_number():
-    response = {
-        'randomNumber': randint(1, 100)
-    }
-    return jsonify(response)
 
-@app.route('/', defaults={'path': ''})
+@app.route('/', defaults={'path':''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
 
-app.run()
+
+# service random number
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 10)
+    }
+    return jsonify(response)
+
+
+app.run(debug=True)
